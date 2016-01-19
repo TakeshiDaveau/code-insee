58f8cd349055c49daa82e7d89d743ca3e8729ca1
TakeshiDaveau <takeshi.daveau@gmail.com>
2016-01-19 21:48:10 +0100

    Merge fil into one

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # code-insee
 
+A very simple library the validate and generate SIREN, SIRET and VAT number. See https://fr.wikipedia.org/wiki/Code_Insee
+
+## How to use
+1. Get code-insee.min.js with `bower install code-insee`
+
+2. Add the library to your project :
+
+        <script src="dist/code-insee.min.js"></script>
+    
+3. Call the different method :
+    
+    - `codeInsee.validateSiren(number)`, `codeInsee.validateVATNumber(number)` for validation
+    - `codeInsee.generateSiren()`, `codeInsee.generateVATNumber()` for generation
---
+++ src/code-insee.js
@@ -0,0 +1,59 @@
+window.codeInsee = (function() {
+    "use strict";
+
+    return {
+        validateSiren: validateSiren,
+        validateVATNumber: validateVATNumber,
+        generateSiren: generateSiren,
+        generateVATNumber: generateVATNumber
+    };
+
+    function validateSiren(number) {
+        var numString = number.toString();
+        var total = 0;
+
+        for (var i = 9; i >= 1; i--) {
+            var value = numString[i - 1] * ((i % 2 === 0) ? 2 : 1);
+            total += ((value < 10) ? value : (1 + value % 10));
+        }
+        return total % 10 === 0;
+    }
+
+    function validateVATNumber(number) {
+        var numString = number.toString();
+        var total = 0;
+
+        for (var i = 9; i >= 1; i--) {
+            var value = numString[i - 1] * ((i % 2 === 0) ? 2 : 1);
+            total += ((value < 10) ? value : (1 + value % 10));
+        }
+        return total % 10 === 0;
+    }
+
+    /*function validateSiret(number) {
+        // TODO
+    }*/
+
+    //
+    // Check that your generate french VAT number isn't use @ : http://ec.europa.eu/taxation_customs/vies/vatResponse.html
+    // Check the VAT number validity @ : http://www.agecsa.com/expert_comptable_siret.html
+    //
+    //
+    function generateSiren() {
+        var number = Math.floor(100000000 + Math.random() * 900000000);
+
+        for (; !validateSiren(number);) {
+            number = Math.floor(100000000 + Math.random() * 900000000);
+        }
+        return number;
+    }
+
+    /*function generateSiret(number) {
+        // TODO
+    }*/
+
+    function generateVATNumber() {
+        var siren = generateSiren();
+        return 'FR' + ((12 + (3 * (+siren % 97))) % 97) + siren;
+    }
+})();
