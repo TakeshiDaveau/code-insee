d2dad5b6948e483eca7cad0bb1965c646bfebfb6
TakeshiDaveau <takeshi.daveau@gmail.com>
2016-01-19 21:47:19 +0100

    Add jshint check

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
-//var jshint = require('gulp-jshint');
+var jshint = require('gulp-jshint');
  
 gulp.task('compress', function() {
     gulp.src('src/*.js')
@@ -8,11 +8,11 @@ gulp.task('compress', function() {
         .pipe(gulp.dest('dist'))
 });
 
-/*gulp.task('lint', function() {
+gulp.task('lint', function() {
     gulp.src('src/*.js')
         .pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter('jshint-stylish')); 
-});*/
+});
 
 
-gulp.task('default', ['compress']);
+gulp.task('default', ['compress', 'lint']);
